2d144a871248ddaa95126820aef193c22643c5e8
mwabonje <ringa.michael@gmail.com>
2023-11-15 17:01:19 +0300

    Assigment on Dart

---
+++ assignment.dart
@@ -0,0 +1,27 @@
+void main() {
+  var x = 50;
+  var y = 69;
+  print(x + y); // addition between the two integers i.e 50 and 69
+  //output 119
+
+  print(x - y); // subtratcion between the two integers
+  //output -19
+
+  print(x * y); // Multiplication between the integers
+  //output 3450
+
+  print(x / y); // Division between the integers
+//output 0.7246376811594203
+
+  //String length counts the number of chracters in a string
+  String stringLength = "Power learn Academy";
+  print(stringLength.length);
+
+  //output 19
+
+  //Getting a first element in a list.
+  var getFirstElement = ["Malindi", 40, "Naivasha", 129.3, 50];
+  print(getFirstElement[0]);
+
+  //Malindi
+}
